864a35b259a488ad5b77e71f6b535d314e708e79
Joshua Mark Manton <joshuamarkk@gmail.com>
2018-11-11 16:28:44 -0800

    new build.bat

--- build.bat
+++ build.bat
@@ -4,16 +4,11 @@ rem echo Running prebuild...
 odin run builder -out=builder.exe
 if exist builder.exe del builder.exe
 
-if exist tmp rmdir /S/Q tmp
-mkdir tmp
-
 if exist out rmdir /S/Q out
 mkdir out
 
 rem echo Copying libs and dlls...
-xcopy /s/e/q includes\windows\libs tmp > NUL
 xcopy /s/e/q includes\windows\runtime out > NUL
-xcopy /s/e/q src tmp > NUL
 
 rem echo Copying resources...
 cd out
@@ -24,13 +19,11 @@ cd ..
 xcopy /s/e/q fonts out\fonts > NUL
 xcopy /s/e/q Resources out\Resources > NUL
 
-cd tmp
-odin build .
+cd src
+odin build . -out=odinscape.exe
 
-ren tmp.exe odinscape.exe
 xcopy /q odinscape.exe ..\out > NUL
 
 cd ..\out
 .\odinscape.exe
 cd ..
-rmdir /s/q tmp
